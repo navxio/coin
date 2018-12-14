a8616becbc63ca38e142c30ca471c411036614ae
Nav Saini <navdeep@mailbox.org>
2018-12-14 18:17:04 +0530

    finish coin dash

--- src/commands/dash.js
+++ src/commands/dash.js
@@ -4,7 +4,6 @@ const fs = require('fs-extra')
 const path = require('path')
 let ccxt = require('ccxt')
 const Table = require('cli-table')
-let resultArray = []
 
 const table = new Table({
   head: ['Cryptocurrency', 'Exchange', 'Amount'],
@@ -12,8 +11,6 @@ const table = new Table({
 
 class DashCommand extends Command {
   async run() {
-    // what's my default currency
-    let CUR = 'USD'
     // start a spinner here
     cli.action.start('Fetching your portfolio')
     let userConfig = null
@@ -32,7 +29,6 @@ class DashCommand extends Command {
         enableRateLimit: true,
       })
       let portfolio = null
-      let krakenTickers = null
       try {
         portfolio = await kraken.fetchBalance()
       } catch (error) {
@@ -42,27 +38,11 @@ class DashCommand extends Command {
       // by passing in an array for argument ['eth/usd', 'xlm/usd']
       if (portfolio) {
         let total = portfolio.total
-        let tickerParam = []
         for (const currency of Object.keys(total)) {
           if (total[currency] > 0) {
-            resultArray.push({symbol: currency, exchange: 'Kraken', amount: total[currency], value: 0})
-            tickerParam.push(currency + '/' + CUR)
+            table.push([currency, 'Kraken', total[currency]])
           }
         }
-        try {
-          krakenTickers = await kraken.fetchTickers(tickerParam)
-        } catch (error) {
-          this.log('something went wrong fetching the kraken tickers')
-        }
-        // do this the functional way
-        let finalArray = resultArray.map(line => {
-          const symbol = line.symbol
-          const ticker = krakenTickers[symbol + '/' + CUR]
-          const average = (ticker.open + ticker.close) / 2
-          line.value = line.amount * average
-          return line
-        })
-        resultArray = finalArray
       }
     }
 
@@ -76,7 +56,6 @@ class DashCommand extends Command {
       })
 
       let portfolio = null
-      let binanceTickers = null
       try {
         portfolio = await binance.fetchBalance()
       } catch (error) {
@@ -84,37 +63,13 @@ class DashCommand extends Command {
       }
       if (portfolio) {
         let total = portfolio.total
-        let tickerParam = []
         for (const currency of Object.keys(total)) {
           if (total[currency] > 0) {
-            resultArray.push({symbol: currency, exchange: 'Binance', amount: total[currency], value: 0})
-            tickerParam.push(currency + '/' + CUR)
+            table.push([currency, 'Binance', total[currency]])
           }
         }
-        try {
-          binanceTickers = await binance.fetchTickers(tickerParam)
-        } catch (error) {
-          this.log('something went wrong fetching the binance tickers')
-        }
-        let binanceArray = resultArray.filter(line => {
-          return line.exchange === 'Binance'
-        })
-        let finalArray = binanceArray.map(line => {
-          const symbol = line.symbol
-          const ticker = binanceTickers[symbol + '/' + CUR]
-          const average = (ticker.open + ticker.close) / 2
-          line.value = line.amount * average
-          return line
-        })
-        resultArray.concat(finalArray)
       }
     }
-    this.log('Result array: ', resultArray)
-
-    resultArray.forEach(line => {
-      table.push([line.symbol, line.exchange, line.amount, line.value])
-    })
-
     cli.action.stop()
     this.log(table.toString())
   }
