6b87a78534e8d459e402ea9eb52048a9aa8363ce
Nav Saini <navdeep@mailbox.org>
2018-12-16 00:05:20 +0530

    implement exchange -a, -e

--- src/commands/exchange.js
+++ src/commands/exchange.js
@@ -1,20 +1,46 @@
 const {Command, flags} = require('@oclif/command')
+const fs = require('fs-extra')
+const path = require('path')
 
 class ExchangeCommand extends Command {
   async run() {
     const {flags} = this.parse(ExchangeCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /home/nav/dev/coin/src/commands/exchange.js`)
+    if (flags.available) {
+      this.log('Kraken\nBinance')
+    }
+    if (flags.enabled) {
+      let enabled = []
+      // read the config file, and find out the available keys
+      try {
+        let userConfig = await fs.readJSON(path.join(this.config.configDir, 'config.json'))
+        if (userConfig.kraken) {
+          enabled.push('Kraken')
+        }
+        if (userConfig.binance) {
+          enabled.push('Binance')
+        }
+        this.log(enabled.join('\n'))
+      } catch (error) {
+        this.log('Error reading config file')
+      }
+    }
+    if (flags.setup) {
+      // inquirer
+    }
+    if (flags.remove) {
+      // inquirer
+    }
   }
 }
 
-ExchangeCommand.description = `Describe the command here
-...
-Extra documentation goes here
+ExchangeCommand.description = `Generic exchange related configuration
 `
 
 ExchangeCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
+  available: flags.boolean({char: 'a', description: 'Supported exchanges'}),
+  enabled: flags.boolean({char: 'e', description: 'Enabled exchanges'}),
+  setup: flags.string({description: 'Set up a new exchange'}),
+  remove: flags.string({description: 'Remove an exchange from coin'}),
 }
 
 module.exports = ExchangeCommand
